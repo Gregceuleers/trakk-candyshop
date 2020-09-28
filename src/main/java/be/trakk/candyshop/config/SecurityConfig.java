19a5d09ed5b2e3643d6955fe229a83525a32cb4b
Ceuleers Grégory <gregory.ceuleers@bstorm.be>
2020-09-28 23:04:06 +0200

    Add SecurityConfig (WIP)

---
+++ src/main/java/be/trakk/candyshop/config/SecurityConfig.java
@@ -0,0 +1,74 @@
+package be.trakk.candyshop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+//                .antMatchers(HttpMethod.POST,
+//                        "/parcours/**",
+//                        "/trous/**",
+//                        "/users/**"
+//                ).hasAuthority("ADMIN")
+                .antMatchers("/h2-console/**").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint((request, response, e) -> {
+                    response.setStatus(401);
+//                    response.setContentType("application/json");
+//                    response.setCharacterEncoding("UTF-8");
+//                    response.getWriter().write(objectMapper.writeValueAsString(new ErrorDTO("Vous devez vous identifier pour pouvoir accéder à " + request.getRequestURI())));
+//                    response.getWriter().close();
+                })
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .cors()
+                // Pour accéder et savoir afficher la page de console H2
+                .and()
+                .headers().frameOptions().disable();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.addAllowedOrigin("http://localhost:4200");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.setAllowCredentials(true);
+
+        return request -> corsConfiguration;
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
